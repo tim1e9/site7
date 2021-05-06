13ff6a021752e0d92e3706366f9826f38df6d807
Tim Crowley <tim@crowleyworks.com>
2021-05-06 14:25:55 -0400

    Included material and basic grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  introtext: {
+    textAlign: 'left',
+    marginLeft: '50px',
+    marginBottom: '100px',
+  }
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit">
+            Photos
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div>
+        <Typography variant="h4">
+          Fishing Tours
+        </Typography>
+      </div>
+      <div>
+        <Typography className={classes.introtext}>
+          Join us for a truly spectacular experience!
+        </Typography>
+      </div>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+            <Typography variant="h6">
+              Inshore Fishing (Half day)
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+        <Paper className={classes.paper}>
+        <Typography variant="h6">
+              Gulf Dolphin Tour (Full day)
+            </Typography>
+            </Paper>
+        </Grid>
+      </Grid>
+    </div>
     </div>
   );
 }
