7e6de33317712d8b695c96bb4a57e506441d225f
Tim Crowley <tim@crowleyworks.com>
2021-05-17 14:47:02 -0400

    Added support for displaying the table

--- src/components/Charters/Charters.js
+++ src/components/Charters/Charters.js
@@ -1,29 +1,62 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-
 import authService from '../../services/authService';
 import charterService from '../../services/chartersService';
- 
+
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+    maxWidth: 700
+  },
+});
+
 export default function Welcome() {
-    // eslint-disable-next-line
-    const [chartersResp, setChartersResp] = React.useState([]);
+
+  const classes = useStyles();
+    const [chartersResp, setChartersResp] = React.useState(null);
 
     React.useEffect( () => {
       const token = authService.getAccessToken();
       charterService.setAccessToken(token);
       async function fetchData() {
-        const result = charterService.getAllCharters();
-        setChartersResp(result);
-        console.log("RESULT: " + JSON.stringify(result));
+        const result = await charterService.getAllCharters();
+        setChartersResp(result.data.body);
         }
       fetchData();
     }, [setChartersResp])
 
     return(
         <div>
-          <Typography variant="h4">
-            Charters for you!
-          </Typography>
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Id</TableCell>
+                  <TableCell align="right">Name</TableCell>
+                  <TableCell align="right">Description</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {chartersResp && chartersResp.map((row) => (
+                  <TableRow key={row.name}>
+                    <TableCell component="th" scope="row">
+                      {row.id}
+                    </TableCell>
+                    <TableCell align="right">{row.name}</TableCell>
+                    <TableCell align="right">{row.descr}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </div>
     );
 }
