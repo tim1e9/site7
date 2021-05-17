44f961fbf48e0b07b1dce2e2ec389a10d5e66f3c
Tim Crowley <tim@crowleyworks.com>
2021-05-17 11:35:23 -0400

    Fixed CORS issue

--- src/services/chartersService.js
+++ src/services/chartersService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const SERVER_URL = "https://9ygu2ddcy0.execute-api.us-east-2.amazonaws.com/crowleyworks-api-deploy-to-dev/charters";
+//const SERVER_URL = "http://localhost:5000/charters";
 let accessToken = null;
 
 const setAccessToken = ( newToken ) => {
@@ -11,7 +12,10 @@ const getAllCharters = async () => {
     const result = await axios.get(
         SERVER_URL, {
             headers: {
-                Authorization: 'Bearer: ' + accessToken
+                Authorization: 'Bearer: ' + accessToken,
+                'Access-Control-Allow-Origin' : '*',
+                'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type'
             }
         }
     );
